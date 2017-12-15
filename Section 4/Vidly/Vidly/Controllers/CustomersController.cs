b162cbdadfb9b78ba3cbae9e43c9a16d28dfb543
Kubing Lin <t6m8@hotmail.com>
2017-12-15 16:48:10 +0800

    Action:New() 傳入viewModel

--- Section 4/Vidly/Vidly/Controllers/CustomersController.cs
+++ Section 4/Vidly/Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -25,7 +26,12 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            return View();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View(viewModel);
         }
 
         // GET: Customers
