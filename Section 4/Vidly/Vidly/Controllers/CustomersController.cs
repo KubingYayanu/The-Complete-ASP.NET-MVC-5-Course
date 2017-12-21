23cc189dda783f4f7c89157a59115c78c4515ceb
Kubing Lin <t6m8@hotmail.com>
2017-12-21 15:53:22 +0800

    1.修改New.cshtml -> CustomerForm.cshtml 2.修改Action: Create -> Save 3.修改ViewModel: NewCustomerViewModel -> CustomerFormViewModel 4.新增Action: Edit 5.修改TextBoxFor:Birthdate的顯示格式 6.新增HiddenFor:Customer.Id 7.Detail.cshtml新增顯示欄位 8.修改Index.cshtml的ActionLink的actionName

--- Section 4/Vidly/Vidly/Controllers/CustomersController.cs
+++ Section 4/Vidly/Vidly/Controllers/CustomersController.cs
@@ -26,18 +26,30 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                //TryUpdateModel(customerInDb, "", new string[] { "Name", "Email "});
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
@@ -55,7 +67,8 @@ namespace Vidly.Controllers
         public ActionResult Details(int id)
         {
             //var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            //var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -71,5 +84,21 @@ namespace Vidly.Controllers
         //        new Customer() { Id = 2, Name = "Mary Williams" }
         //    };
         //}
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
     }
 }
