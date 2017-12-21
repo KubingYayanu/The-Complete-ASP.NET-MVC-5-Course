7124b79cf49bd6088cba50548580d5014bfc20ae
Kubing Lin <t6m8@hotmail.com>
2017-12-21 17:25:17 +0800

    1.增加建構子及DbContext 2.增加Action: New 3.修改Action: Edit、Index

--- Section 4/Vidly/Vidly/Controllers/MoviesController.cs
+++ Section 4/Vidly/Vidly/Controllers/MoviesController.cs
@@ -10,6 +10,19 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -44,21 +57,69 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres,
+                FormName = "New Movie"
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                 
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
         public ActionResult Edit(int id)
         {
-            return Content("id=" + id);
+            //return Content("id=" + id);
+
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
         }
 
         // movies
         public ActionResult Index(int? pageIndex, string sortBy)
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+            //if (!pageIndex.HasValue)
+            //    pageIndex = 1;
 
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+            //if (string.IsNullOrWhiteSpace(sortBy))
+            //    sortBy = "Name";
 
-            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+            //return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+
+            var movies = _context.Movies.ToList();
+
+            return View(movies);
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
