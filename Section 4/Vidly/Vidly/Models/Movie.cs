fbbca5ea6e16e269ca6a4bc4c1b66005e7315e1e
Kubing Lin <t6m8@hotmail.com>
2017-12-21 17:23:25 +0800

    稱加欄位

--- Section 4/Vidly/Vidly/Models/Movie.cs
+++ Section 4/Vidly/Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,22 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Required]
+        [Display(Name = "Number in Stock")]
+        public int NumberInStock { get; set; }
     }
 }
