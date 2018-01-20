cb2ad298406a8fa6de480a5ac995ae494d5b560b
Kubing Lin <t6m8@hotmail.com>
2018-01-20 14:36:13 +0800

    1.add action: Index 2.initialize customer instance in action: New 3.add action : Edit 4.add ModelState validate in action: Save

--- Section 4/Vidly/Vidly/Controllers/CustomersController.cs
+++ Section 4/Vidly/Vidly/Controllers/CustomersController.cs
@@ -23,20 +23,58 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+        // GET: Customers
+        public ActionResult Index()
+        {
+            //var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
+        }
+
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
             return View("CustomerForm", viewModel);
         }
 
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
             else
@@ -53,15 +91,6 @@ namespace Vidly.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
-        }
-
-        // GET: Customers
-        public ActionResult Index()
-        {
-            //var customers = GetCustomers();
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-
-            return View(customers);
         }
 
         public ActionResult Details(int id)
@@ -84,21 +113,5 @@ namespace Vidly.Controllers
         //        new Customer() { Id = 2, Name = "Mary Williams" }
         //    };
         //}
-
-        public ActionResult Edit(int id)
-        {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
-
-            if (customer == null)
-                return HttpNotFound();
-
-            var viewModel = new CustomerFormViewModel
-            {
-                Customer = customer,
-                MembershipTypes = _context.MembershipTypes.ToList()
-            };
-
-            return View("CustomerForm", viewModel);
-        }
     }
 }
