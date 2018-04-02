b8bfeb68f124a21d2e5a9d22d0208e679eb99718
Kubing Lin <t6m8@hotmail.com>
2018-04-02 11:39:47 +0800

    Initiate Section 6

---
+++ Section 6/Vidly/Vidly/Migrations/201712210850174_AddColumnsForMovies.cs
@@ -0,0 +1,26 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddColumnsForMovies : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "GenreId", c => c.Byte(nullable: false));
+            AddColumn("dbo.Movies", "ReleaseDate", c => c.DateTime());
+            AddColumn("dbo.Movies", "NumberInStock", c => c.Int(nullable: false));
+            CreateIndex("dbo.Movies", "GenreId");
+            AddForeignKey("dbo.Movies", "GenreId", "dbo.Genres", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "GenreId", "dbo.Genres");
+            DropIndex("dbo.Movies", new[] { "GenreId" });
+            DropColumn("dbo.Movies", "NumberInStock");
+            DropColumn("dbo.Movies", "ReleaseDate");
+            DropColumn("dbo.Movies", "GenreId");
+        }
+    }
+}
