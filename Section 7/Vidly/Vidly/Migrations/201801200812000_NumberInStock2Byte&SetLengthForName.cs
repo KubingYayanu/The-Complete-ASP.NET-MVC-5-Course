d04c9fef0e67d92a2c698d810e5193dcab57860c
Kubing Lin <t6m8@hotmail.com>
2018-04-07 23:30:41 +0800

    Initial Section 7

---
+++ Section 7/Vidly/Vidly/Migrations/201801200812000_NumberInStock2Byte&SetLengthForName.cs
@@ -0,0 +1,21 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NumberInStock2ByteSetLengthForName : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("ALTER TABLE dbo.Movies DROP CONSTRAINT DF__Movies__NumberIn__6EF57B66");
+            AlterColumn("dbo.Movies", "Name", c => c.String(nullable: false, maxLength: 255));
+            AlterColumn("dbo.Movies", "NumberInStock", c => c.Byte(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "NumberInStock", c => c.Int(nullable: false));
+            AlterColumn("dbo.Movies", "Name", c => c.String(nullable: false));
+        }
+    }
+}
