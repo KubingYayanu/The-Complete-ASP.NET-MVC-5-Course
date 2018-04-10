e211bccd90b6c21220610f3f1da6af4b084dba0e
Kubing Lin <t6m8@hotmail.com>
2018-04-10 23:11:25 +0800

    Add Phone field to Profile Data

--- Section 8/Vidly/Vidly/Controllers/MoviesController.cs
+++ Section 8/Vidly/Vidly/Controllers/MoviesController.cs
@@ -69,6 +69,7 @@ namespace Vidly.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public ActionResult Save(Movie movie)
         {
             if (!ModelState.IsValid)
@@ -98,6 +99,7 @@ namespace Vidly.Controllers
             return RedirectToAction("Index", "Movies");
         }
 
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public ActionResult Edit(int id)
         {
             //return Content("id=" + id);
--- Section 8/Vidly/Vidly/Models/ApplicationUser.cs
+++ Section 8/Vidly/Vidly/Models/ApplicationUser.cs
@@ -1,15 +1,18 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Vidly.Models
 {
     // 您可以在 ApplicationUser 類別新增更多屬性，為使用者新增設定檔資料，請造訪 http://go.microsoft.com/fwlink/?LinkID=317594 以深入了解。
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
+
         [Required]
         [StringLength(255)]
         public string DrivingLicense { get; set; }
@@ -20,27 +23,6 @@ namespace Vidly.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // 在這裡新增自訂使用者宣告
             return userIdentity;
-        }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public DbSet<Customer> Customers { get; set; }
-
-        public DbSet<Movie> Movies { get; set; }
-
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-
-        public DbSet<Genre> Genres { get; set; }
-
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
         }
     }
 }
