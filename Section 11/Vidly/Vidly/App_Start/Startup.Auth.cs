016afa8387e62dfd0b5ac693ca28e70187d46838
Kubing Lin <t6m8@hotmail.com>
2018-04-14 22:43:32 +0800

    Add Testing Configuration

--- Section 11/Vidly/Vidly/App_Start/Startup.Auth.cs
+++ Section 11/Vidly/Vidly/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using Vidly.Models;
+using System.Configuration;
 
 namespace Vidly
 {
@@ -55,8 +56,8 @@ namespace Vidly
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "1674334969349906",
-               appSecret: "89c25d85b751577aaf243fb52067cdcd");
+               appId: ConfigurationManager.AppSettings["FacebookAppId"],
+               appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"]);
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
