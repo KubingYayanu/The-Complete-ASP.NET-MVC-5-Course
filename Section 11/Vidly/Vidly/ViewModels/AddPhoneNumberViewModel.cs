187649b62c8f617a660f0eedfd28747d86e97ea7
Kubing Lin <t6m8@hotmail.com>
2018-04-14 21:53:03 +0800

    Initial Section 11

---
+++ Section 11/Vidly/Vidly/ViewModels/AddPhoneNumberViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.ViewModels
+{
+    public class AddPhoneNumberViewModel
+    {
+        [Required]
+        [Phone]
+        [Display(Name = "電話號碼")]
+        public string Number { get; set; }
+    }
+}
---
+++ Section 11/Vidly/Vidly/ViewModels/jQuery.DataTables/DataTableOrderModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.ViewModels.jQuery.DataTables
+{
+    public class DataTableOrderModel
+    {
+        public int column { get; set; }
+        public string dir { get; set; }
+    }
+}
