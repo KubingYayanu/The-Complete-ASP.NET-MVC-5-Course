4e9c827a97b7184397abfe1256ebe6bd1c9d7dbc
Kubing Lin <kubing.lin@macrowell.com.tw>
2018-06-01 17:33:26 +0800

    Install Owin Jwt middleware

---
+++ Section 11/Vidly/Vidly/App_Start/Startup.Jwt.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using System.IdentityModel.Tokens;
+
+namespace Vidly
+{
+    public partial class Startup
+    {
+        public void ConfigureJwt(IAppBuilder app)
+        {
+            string audience = ConfigurationManager.AppSettings["JwtAudience"];
+            string issuer = ConfigurationManager.AppSettings["JwtIssuer"];
+            string signKey = ConfigurationManager.AppSettings["JwtSignKey"];
+
+            app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                AllowedAudiences = new[] { audience },
+                IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                {
+                    new SymmetricKeyIssuerSecurityTokenProvider(issuer, TextEncodings.Base64Url.Decode(signKey))
+                },
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidAudiences = new[] { audience },
+                    ValidIssuer = issuer,
+                    IssuerSigningKey = new InMemorySymmetricSecurityKey(TextEncodings.Base64Url.Decode(signKey)),
+                    IssuerSigningTokens = new SymmetricKeyIssuerSecurityTokenProvider(issuer, signKey).SecurityTokens
+                }
+            });
+        }
+    }
+}
