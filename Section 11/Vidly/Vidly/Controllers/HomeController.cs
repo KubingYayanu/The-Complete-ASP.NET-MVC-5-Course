1a0c0384d8352181eeaf4ff793781e59aa140534
Kubing Lin <t6m8@hotmail.com>
2018-07-08 18:05:56 +0800

    實作google reCAPTCHA

--- Section 11/Vidly/Vidly/Controllers/HomeController.cs
+++ Section 11/Vidly/Vidly/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using GoogleRecaptcha;
 using System.Web.Mvc;
 
 namespace Vidly.Controllers
@@ -24,6 +21,28 @@ namespace Vidly.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult PostIndex()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PostIndex(FormCollection form)
+        {
+            IRecaptcha<RecaptchaV2Result> recaptcha = new RecaptchaV2(new RecaptchaV2Data
+            {
+                Secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
+            });
+
+            var result = recaptcha.Verify();
+            if (result.Success)
+            {
+                //TODO: do the thing
+            }
 
             return View();
         }
